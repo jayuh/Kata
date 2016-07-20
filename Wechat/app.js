bccfb7a6341ba786eff71959cfa13659e651cd0c
Jayuh <designer.shen@gmail.com>
2016-07-21 00:09:32 +0800

    获取access_token

--- Wechat/app.js
+++ Wechat/app.js
@@ -3,15 +3,17 @@
  */
 
 var http = require('http');
+var https = require('https');
 var url = require('url');
 var querystring = require('querystring');
 var crypto  = require('crypto');
 
-var WECHATCONFIG = {
+var wechatConfig = {
     appid : 'wx29564eaf67e22491',
     appsecret : '26d6677307ba0bf5dfa45a0b8a0f958b',
     token : 'Pa888888',
-    wechatUrl : 'http://sz.api.weixin.qq.com'
+    wechatUrl : 'sz.api.weixin.qq.com',
+    access_token : ''
 }
 
 http.createServer(function(res, req) {
@@ -23,7 +25,7 @@ http.createServer(function(res, req) {
         // 验证服务器地址的有效性
         // http://28cf2399.ngrok.natapp.cn/checkSignature?signature=f31b8b749778cffabe02e004b0a4d363db389545&timestamp=aaa&nonce=bbb&echostr=abc
         var result = '';
-        var token = WECHATCONFIG.token;
+        var token = wechatConfig.token;
         var signature = urlQuery.signature;
         var timestamp = urlQuery.timestamp;
         var nonce = urlQuery.nonce;
@@ -41,4 +43,50 @@ http.createServer(function(res, req) {
 }).listen(3000);
 
 console.log('服务器已启动，地址是：http://127.0.0.1:3000');
+
+// 获取access_token
+// https://api.weixin.qq.com/cgi-bin/token?grant_type=client_credential&appid=APPID&secret=APPSECRET
+function getAAccessToken() {
+    var requestUrl = 'https://' + wechatConfig.wechatUrl + '/cgi-bin/token?grant_type=client_credential&appid=' + wechatConfig.appid + '&secret=' + wechatConfig.appid;
+    var options = {
+        host: wechatConfig.wechatUrl,
+        path: '/cgi-bin/token?' + querystring.stringify({
+            grant_type: 'client_credential',
+            appid: wechatConfig.appid,
+            secret: wechatConfig.appsecret
+        })
+    };
+    https.get(options, function(res) {
+        var body = '';
+        res.on('data', function(d) {
+            body += d;
+        });
+        res.on('end', function() {
+            var parsed = JSON.parse(body);
+            if (!parsed.errcode) {
+                wechatConfig.access_token = parsed.access_token;
+                console.log('access_token变更为: ' + wechatConfig.access_token);
+                setTimeout(getAAccessToken, (parsed.expires_in || 7200) * 1000 - 200 * 1000);
+            } else {
+                console.log('获取access_token失败,详情如下: ');
+                console.log(parsed);
+            }
+        });
+    }).on('error', function(e) {
+        console.error(e);
+    });
+}
+getAAccessToken();
+
+
+
+
+
+
+
+
+
+
+
+
 
