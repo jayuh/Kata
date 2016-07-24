87042235f2b770c18c03eae67ac6fa2c665b63fa
Jayuh <designer.shen@gmail.com>
2016-07-24 23:55:51 +0800

    no comment

--- Wechat/app.js
+++ Wechat/app.js
@@ -6,40 +6,86 @@ var http = require('http');
 var https = require('https');
 var url = require('url');
 var querystring = require('querystring');
-var crypto  = require('crypto');
+var xml2js = require('xml2js');
 
+// 测试账号信息
 var wechatConfig = {
     appid : 'wx29564eaf67e22491',
     appsecret : '26d6677307ba0bf5dfa45a0b8a0f958b',
     token : 'Pa888888',
     wechatUrl : 'sz.api.weixin.qq.com',
-    access_token : ''
+    access_token : 'sTskAZw0XYGBbISSrENlo_UvOCba69CGj2LtZAe5LhVbjadj8fwRIKXXiv1Wtv4JQ9vJ8aoCx5ARX_UnubWVubgOC5f-dLscEimDlZ8zQMdouaNnaiHlPd2bho5pFdOWBJJfACATBB'
 }
 
-http.createServer(function(res, req) {
-    var urlParse = url.parse(res.url);
+// 生产账号信息
+// var wechatConfig = {
+//     appid : 'wx584b6315fe78a459',
+//     appsecret : 'bdc60da97c264f7d6030060de7f4c27e',
+//     token : 'Pa888888',
+//     wechatUrl : 'sz.api.weixin.qq.com',
+//     access_token : 'sTskAZw0XYGBbISSrENlo_UvOCba69CGj2LtZAe5LhVbjadj8fwRIKXXiv1Wtv4JQ9vJ8aoCx5ARX_UnubWVubgOC5f-dLscEimDlZ8zQMdouaNnaiHlPd2bho5pFdOWBJJfACATBB'
+// }
+
+http.createServer(function(req, res) {
+    var urlParse = url.parse(req.url);
     var urlPathname = urlParse.pathname;
     var urlQuery = querystring.parse(urlParse.query);
 
-    if (urlPathname === '/checkSignature') {
-        // 验证服务器地址的有效性
-        // http://28cf2399.ngrok.natapp.cn/checkSignature?signature=f31b8b749778cffabe02e004b0a4d363db389545&timestamp=aaa&nonce=bbb&echostr=abc
-        var result = '';
-        var token = wechatConfig.token;
-        var signature = urlQuery.signature;
-        var timestamp = urlQuery.timestamp;
-        var nonce = urlQuery.nonce;
-        var str = [token, timestamp, nonce].sort().join('');
-        var selfSignature = crypto.createHash('sha1').update(str).digest('hex');
-        if (selfSignature === signature) {
-            result = urlQuery.echostr;
-        }
-        req.writeHead(200, {'Content-Type': 'text/plain'});
-        req.end(result);
-    } else {
-        req.writeHead(200, {'Content-Type': 'text/plain'});
-        req.end('404');
-    }
+    // 自动回复
+    var content = '';
+    req.on('data', function (chunk) {
+        content += chunk;
+    });
+    req.on('end', function () {
+        xml2js.parseString(content, function(err, result) {
+            console.log('将xml转换为JS对象');
+            console.log(result.xml);
+            var obj = {
+                ToUserName: result.xml.FromUserName[0],
+                FromUserName: result.xml.ToUserName[0],
+                CreateTime: +new Date(),
+                MsgType: 'text',
+                Content: '[自动回复]' + result.xml.Content[0]
+            }
+            var builder = new xml2js.Builder({
+                rootName: 'xml',
+                cdata: true
+            });
+            var xml = builder.buildObject(obj);
+            res.writeHead(200, {'Content-Type': 'text/plain'});
+            res.end(xml);
+        });
+    });
+
+    // if (urlPathname === '/checkSignature') {
+    //     // 验证服务器地址的有效性
+    //     // http://28cf2399.ngrok.natapp.cn/checkSignature?signature=f31b8b749778cffabe02e004b0a4d363db389545&timestamp=aaa&nonce=bbb&echostr=abc
+    //     var result = '';
+    //     var token = wechatConfig.token;
+    //     var signature = urlQuery.signature;
+    //     var timestamp = urlQuery.timestamp;
+    //     var nonce = urlQuery.nonce;
+    //     var str = [token, timestamp, nonce].sort().join('');
+    //     var selfSignature = crypto.createHash('sha1').update(str).digest('hex');
+    //     if (selfSignature === signature) {
+    //         result = urlQuery.echostr;
+    //     }
+    //     res.writeHead(200, {'Content-Type': 'text/plain'});
+    //     res.end(result);
+    // } else if (urlPathname === '/textMsg') {
+    //     var content = '';
+    //     req.on('data', function (chunk) {
+    //         content += chunk;
+    //     });
+    //     req.on('end', function () {
+    //         console.log(content);
+    //     });
+    //     res.writeHead(200, {'Content-Type': 'text/plain'});
+    //     res.end('success');
+    // } else {
+    //     res.writeHead(200, {'Content-Type': 'text/plain'});
+    //     res.end('404');
+    // }
 }).listen(3000);
 
 console.log('服务器已启动，地址是：http://127.0.0.1:3000');
@@ -76,7 +122,7 @@ function getAAccessToken() {
         console.error(e);
     });
 }
-getAAccessToken();
+// getAAccessToken();
 
 
 
